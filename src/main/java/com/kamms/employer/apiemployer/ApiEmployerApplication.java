f290a5f23a750448830778d8113ccc084fe04c6b
Prapurna <pmanda1@sapient.com>
2018-09-02 11:37:46 +0530

    Initial commit

---
+++ src/main/java/com/kamms/employer/apiemployer/ApiEmployerApplication.java
@@ -0,0 +1,12 @@
+package com.kamms.employer.apiemployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmployerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kamms/employer/apiemployer/ApiEmployerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamms.employer.apiemployer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiEmployerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
